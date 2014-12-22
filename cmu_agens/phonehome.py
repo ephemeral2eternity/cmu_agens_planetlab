f7e8ffa2f5ee04a63af63e482bcaabb1f0441959
Chen Wang <chenw@andrew.cmu.edu>
2014-12-22 12:33:32 +0000

    deploy helloworld example in planetlab

---
+++ cmu_agens/phonehome.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+### phonehome.py
+### Hello World demonstration script
+phonehome_url = "http://146.148.66.148/phonehome.php"
+import sys, urllib, xmlrpclib, socket
+
+# PlanetLab PLCAPI url
+plc_host = 'www.planet-lab.org'
+api_url = "https://%s:443/PLCAPI/"%plc_host
+
+## Get PLC_API
+plc_api = xmlrpclib.ServerProxy(api_url, allow_none=True)
+
+# Anonymous user to retrieve node name
+auth = {}
+auth['AuthMethod'] = "anonymous"
+auth['Role'] = "user"
+hostname = socket.gethostname()
+query = plc_api.GetNodes(auth, {'hostname': hostname}, ['site_id'])
+
+site_id = query[0]['site_id']
+site_info = api_server.GetSites(auth, {'site_id': site_id}, ['site_id', 'name', 'url','latitude', 'longitude', 'login_base'])
+site_info = urllib.urlencode(site_info[0])
+urllib.urlopen(phonehome_url, site_info)
