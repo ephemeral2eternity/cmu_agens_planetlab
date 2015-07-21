6d7c329827db310d7fecc22dde1c3f888056cba6
Chen Wang <chenw@andrew.cmu.edu>
2015-07-21 21:34:55 +0000

    Backup the accessible client node list for port 8717

---
+++ cmu_agens/coop-client/coop_agent.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python
+# Cooperative Client Agent in QoE based Anomaly Localization System
+# coop_agent.py
+# Chen Wang, chenw@cmu.edu
+# Jun. 16, 2015
+from os import curdir, sep
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import os # os. path
+from attach_cache_agent import *
+from test_utils import *
+from coop_utils import *
+import urlparse
+
+## Globe Variables
+PORT = 8717
+hostname = getMyName()
+
+# Get Trace Routes from all servers
+all_hops = get_all_routes()
+
+#==========================================================================================
+# Write out an welcome page showing all routes to all cache agents deployed in Google Cloud
+#==========================================================================================
+def welcome_page():
+    part1 = "<html>  \
+                <title>  \
+                    Client Peer " + hostname + "Listening on Port: " + str(PORT) + " \
+                </title> \
+                <body>  \
+                	<h1>Client Peer " + hostname + "Listening on Port: " + str(PORT) + "</h1> \
+                    <h1> The routes from client " + hostname + "to all servers include followings: </h1> \
+                    <ul>"
+
+    part2 = ""
+    for srv in all_hops.keys():
+    	part2 = part2 + "<li>" + srv + "<ul>"
+    	cur_hops = all_hops[srv]
+    	for hop_id in sorted(cur_hops.keys()):
+    		part2 = part2 + "<li>" + str(hop_id)  + ", " + cur_hops[hop_id]['Name'] + "</li>"
+
+    	part2 = part2 + "</ul></li>"
+
+    part3 = "</ul> \
+                </body> \
+            </html>"
+
+    page = part1 + part2 + part3
+    return page
+
+#==========================================================================================
+# Handle peer client's requests on querying the route info and QoE
+#==========================================================================================
+class MyHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			if "ico" in self.command:
+				return
+
+			## Show all the traceroute info to all servers
+			elif self.path == '/':
+				page = welcome_page()
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+				self.wfile.write(page)
+				return
+
+			## Get the QoE, video, and route info from a peer client
+			elif self.path.startswith('/get?'):
+				vidID = int(self.path.split('?')[1])
+
+				info = get_info(vidID)
+
+				pkt = wrapPKT(info)
+
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.send_header('Params', json.dumps(pkt))
+				self.end_headers()
+
+				cur_page = "<h1>The packet sent to other clients for anomaly localization is: </h1>"
+				cur_page = cur_page + "<p>" + json.dumps(pkt) + "</p>"
+				self.wfile.write(cur_page)
+				return
+
+			## Get the latest observation from local client
+			elif self.path.startswith('/latest'):
+				info = get_latest()
+				pkt = wrapPKT(info)
+
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.send_header('Params', json.dumps(pkt))
+				self.end_headers()
+
+				cur_page = "<h1>The packet sent to other clients for anomaly localization is: </h1>"
+				cur_page = cur_page + "<p>" + json.dumps(pkt) + "</p>"
+				self.wfile.write(cur_page)
+				return
+
+			else:
+				return
+
+		except IOError as e :     
+			print e
+			self.send_error(404,'File Not Found: %s' % self.path)
+
+	def do_POST(self):
+		self.send_response(200)
+		self.end_headers()
+		self.wfile.write("<HTML><HEAD></HEAD><BODY>POST OK.<BR><BR>");
+		self.wfile.write( "File uploaded under name: " + os.path.split(fullname)[1] );
+		self.wfile.write(  '<BR><A HREF=%s>back</A>' % ( UPLOAD_PAGE, )  )
+		self.wfile.write("</BODY></HTML>");
+
+#==========================================================================================
+# Wrap info dict obtained from reading qoe.db to a packet to send over http
+#==========================================================================================
+def wrapPKT(info):
+	if not info:
+		pkt = {}
+	else:
+		srv = info['srv']
+		srv_qoe = info['qoe']
+		srv_name = info['srvName']
+		vidID = info['video']
+		srv_route_str = get_route_str(srv, all_hops)
+
+		## Return the anomaly info for cooperative anomaly localization
+		pkt = dict()
+		pkt['qoe'] = str(srv_qoe)
+		pkt['route'] = srv_route_str
+		pkt['video'] = vidID
+		pkt['srv'] = srv
+		pkt['srvName'] = srv_name
+
+	return pkt
+
+#==========================================================================================
+# Main Function of A Cooperative Agent
+#==========================================================================================
+def main(argv):
+	### Get client name and attache to the closest cache agent
+	client_name = getMyName()
+	cache_agent = attach_cache_agent()
+
+	## Connect client to its cache agent and update centralized manager
+	print "Client ", client_name, " is connecting to cache agent : ", cache_agent['name']
+	connect_cache_agent(client_name, cache_agent['name'], cache_agent['ip'])
+	update_cache_agent(client_name, cache_agent['name'])
+	create_db()
+
+	## Start listening as a server on PORT
+	server = HTTPServer(('', PORT), MyHandler)
+	print 'started listening on port : ', str(PORT), ' .... '
+	server.serve_forever()
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ cmu_agens/coop-client/coop_utils.py
@@ -0,0 +1,155 @@
+## Some utilities for cooperative agents
+# Chen Wang, Jun. 17, 2015
+# chenw@andrew.cmu.edu
+## coop_utils.py
+import random
+import time
+import datetime
+import math
+import os
+import urllib2
+import sqlite3 as lite
+from attach_cache_agent import *
+from traceroute import *
+from sys_traceroute import *
+
+## ======================================================================== 
+# Create and Initialize a database to keep
+## ========================================================================
+def create_db():
+	full_path = os.path.realpath(__file__)
+	path, fn = os.path.split(full_path)
+	db_name = path + "/qoe.db"
+
+	# Create or connect to the denoted database
+	con = lite.connect(db_name)
+	cur = con.cursor()
+
+	# Check if there exists a table called QoE.
+	cur.execute("CREATE TABLE IF NOT EXISTS QoE(vidID int, srvName text, srvIP text, QoE real, TS datetime)")
+	cur.execute("INSERT INTO QoE(vidID, srvName, srvIP, QoE, TS) values (?, ?, ?, ?, ?)", (1, 'cache-01', '104.197.42.89', 5.0, datetime.datetime.now()))
+	cur.execute("INSERT INTO QoE(vidID, srvName, srvIP, QoE, TS) values (?, ?, ?, ?, ?)", (1, 'cache-02', '104.197.59.54', 5.0, datetime.datetime.now()))
+	cur.execute("INSERT INTO QoE(vidID, srvName, srvIP, QoE, TS) values (?, ?, ?, ?, ?)", (1, 'cache-03', '104.197.8.50', 5.0, datetime.datetime.now()))
+	con.commit()	
+
+	con.close()
+
+## ======================================================================== 
+# Connect the client to its closest cache agent
+# @input : client ---- The client name
+#		   cache_agent ---- The cache agent the client is connecting to
+## ========================================================================
+def connect_cache_agent(client, cache_agent, cache_agent_ip):
+	update_url = "http://%s:8615/client/add?%s" % (cache_agent_ip, client)
+	try:
+		rsp = urllib2.urlopen(update_url)
+		print rsp
+		print "Connect client :", client, " to its cache agent ", cache_agent, " successfully!"
+	except:
+		print "Failed to connect client ", client, " to its cache_agent", cache_agent, "!"
+
+#==========================================================================================
+# Get Traceroutes to all available servers
+#==========================================================================================
+def get_all_routes():
+	## Try several times before exit
+	all_cache_agents = get_cache_agents()
+	trial_num = 0
+	while not all_cache_agents and trial_num < 10:
+		all_cache_agents = get_cache_agents()
+		trial_num = trial_num + 1
+
+	all_srv_hops = dict()
+	for srv in all_cache_agents.keys():
+		# srv_hops = traceroute(all_cache_agents[srv])
+		srv_hops = sys_traceroute(all_cache_agents[srv])
+		all_srv_hops[all_cache_agents[srv]] = srv_hops
+
+	return all_srv_hops
+
+#==========================================================================================
+# Read the client's latest QoE and streaming server address for a certain video
+#==========================================================================================
+def get_info(vidID):
+	full_path = os.path.realpath(__file__)
+	path, fn = os.path.split(full_path)
+	db_name = path + "/qoe.db"
+
+	con = lite.connect(db_name)
+	cur = con.cursor()
+
+	SELECT_CMD = "SELECT * FROM QoE WHERE vidID=%d ORDER BY TS DESC LIMIT 1" % vidID
+	cur.execute(SELECT_CMD)
+	lite_info = cur.fetchall()
+	con.close()
+
+	## Read the server name, server address and the video ID.
+	if len(lite_info) > 0:
+		srv = lite_info[0][2]
+		video = lite_info[0][0]
+		qoe = lite_info[0][3]
+		srv_name = lite_info[0][1]
+
+		info = dict()
+		info['srv'] = srv
+		info['srvName'] = srv_name
+		info['qoe'] = float(qoe)
+		info['video'] = video
+
+	## Should add code to handle empty info.
+	else:
+		info = {}
+
+	return info
+
+#==========================================================================================
+# Read the client's latest QoE and streaming server address from qoe.db
+#==========================================================================================
+def get_latest():
+	full_path = os.path.realpath(__file__)
+	path, fn = os.path.split(full_path)
+	db_name = path + "/qoe.db"
+
+	con = lite.connect(db_name)
+	cur = con.cursor()
+
+	SELECT_CMD = "SELECT * FROM QoE ORDER BY TS DESC LIMIT 1"
+	cur.execute(SELECT_CMD)
+	lite_info = cur.fetchall()
+	con.close()
+
+	## Read the server name, server address and the video ID.
+	if len(lite_info) > 0:
+		## Read the server name, server address and the video ID.
+		srv = lite_info[0][2]
+		video = lite_info[0][0]
+		qoe = lite_info[0][3]
+		srv_name = lite_info[0][1]
+
+		info = dict()
+		info['srv'] = srv
+		info['srvName'] = srv_name
+		info['qoe'] = float(qoe)
+		info['video'] = video
+
+	else:
+		info = {}
+
+	return info
+
+
+#==========================================================================================
+# Get the string of route info from all routes
+# @input: srv ---- the server the client is streaming from
+#         all_routes ---- all traceroute info from client to the streaming server, srv
+# @return : the string of all hops in the route from the client to the streaming server
+#==========================================================================================
+def get_route_str(srv, all_routes):
+	cur_route = all_routes[srv]
+
+	cur_route_list = []
+	for hop_id in cur_route.keys():
+		cur_route_list.append(cur_route[hop_id]['Name'])
+
+	cur_route_str = ', '.join(cur_route_list)
+	return cur_route_str
